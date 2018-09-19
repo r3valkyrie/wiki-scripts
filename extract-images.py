aa8e9bafccd3616e780bae46afe4562f8cfd582b
r3valkyrie <valkyrie@valkyrie.sexy>
2018-09-19 05:05:15 -0400

    fixed weird directory-related issue

--- extract-images.py
+++ extract-images.py
@@ -27,6 +27,7 @@ def main():
 def nice(meme):
     # wiki_url = sys.argv[1] # Removing temporarily
     img_dir = os.path.dirname(os.path.realpath(__file__)) + "/extracted-images/"
+    print(img_dir)
     if os.path.exists(img_dir):
         url_extract(meme, img_dir)
     else:
@@ -36,28 +37,28 @@ def nice(meme):
 # Extract image urls from parsed html.
 
 def url_extract(url, img_dir):
-    os.chdir(img_dir)
     print('!! DOWNLOADING FROM ' + url + ' !!')
     response = urllib.request.urlopen(url, data=None)
     html = response.read()
     soup = BeautifulSoup(html, "html.parser")
     images = [img['src'] for img in soup.find_all('img')]
 
-    format_attrs(images)
+    format_attrs(images, img_dir)
 
 # Append the image path to the url, forming a direct link to the image.
-def format_attrs(images):
+def format_attrs(images, img_dir):
     images_formatted = []
     for img in images:
         images_formatted.append("https://tgstation13.org" + img)
 
-    dl_from_array(images_formatted)
+    dl_from_array(images_formatted, img_dir)
 
 # Download our images via direct link.
-def dl_from_array(images_formatted):
+def dl_from_array(images_formatted, img_dir):
     for url in images_formatted:
         filename = url.rsplit('/', 1)[-1]
-        directory = url.rsplit('/', 3)[-3] + '/' + url.rsplit('/', 2)[-2] + '/'
+        directory = img_dir + url.rsplit('/', 3)[-3] + '/' + url.rsplit('/', 2)[-2] + '/'
+        print(directory)
         if not os.path.exists(directory):
             os.makedirs(directory)
         if not os.path.exists(directory + filename):
